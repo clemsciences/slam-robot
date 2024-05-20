aa6499a379fe75dc8823e7bd125b14137611a5d0
Clément Besnier <clem@clementbesnier.fr>
2024-05-20 21:38:19 +0200

    Improved world_1 example

--- slam_robot/examples/world_1.py
+++ slam_robot/examples/world_1.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from slam_robot.models.action import Turn, Move, Sense
 from slam_robot.models.robot import Robot
 from slam_robot.models.world import World
 from slam_robot.models.world_items import Circle, LineByTwoPoints
@@ -7,6 +8,7 @@ from slam_robot.utils.geometry import Point
 
 import matplotlib.pyplot as plt
 
+plt.rcParams["figure.figsize"] = (5, 5)
 
 world_1 = World(
     [
@@ -22,31 +24,44 @@ world_1 = World(
 
 robot = Robot(Point(12, 12), 0)
 
-what_the_robot_sees = robot.sense(world_1)
-print(what_the_robot_sees)
+actions = [
+    Turn.from_objective(1.5, np.pi/3),
+    Sense(),
+    Move.from_objective(10, 10),
+    Sense(),
+    Turn.from_objective(1.5, np.pi/6),
+    Move.from_objective(10, 30),
+    Sense(),
+    Move.from_objective(10, 30),
+    Sense()
+]
 
+robot.apply_actions(actions, world_1)
 
-fix, ax = plt.subplots()
+for measure in robot.measures:
+    fix, ax = plt.subplots()
 
-world_1.draw(ax)
-robot.draw(ax)
+    world_1.draw(ax)
 
-x_values = []
-y_values = []
-for point in what_the_robot_sees:
-    line = LineByTwoPoints(robot.position, point)
-    line.draw(ax)
-    # x_values.append(point.x + robot.position.x)
-    x_values.append(point.x)
-    # y_values.append(point.y + robot.position.y)
-    y_values.append(point.y)
-plt.scatter(x_values, y_values, color="blue")
-print([(x_values[i], y_values[i]) for i in range(len(x_values))])
+    x_values = []
+    y_values = []
+    print(measure.position)
+    for point in measure.obstacles:
+        line = LineByTwoPoints(measure.position, point)
+        # line.draw(ax)
+        # x_values.append(point.x + robot.position.x)
+        x_values.append(point.x)
+        # y_values.append(point.y + robot.position.y)
+        y_values.append(point.y)
+    plt.scatter(x_values, y_values, color="blue")
+    # print([(x_values[i], y_values[i]) for i in range(len(x_values))])
+    # robot.draw(ax)
+    plt.scatter([measure.position.x], [measure.position.y], color="red")
 
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("Plot")
-plt.show()
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Plot")
+    plt.show()
 
 
 
